4a22859daaa5f93135190393c35c79a035134ccf
Michael Wynholds <mike@carbonfive.com>
2011-08-24 15:20:43 -0700

    Config.init

--- lib/southy/config.rb
+++ lib/southy/config.rb
@@ -1,12 +1,26 @@
 require 'tmpdir'
+require 'yaml'
 
 class Southy::Config
   def initialize(config_dir = nil)
     @config_dir = config_dir || Dir.mktmpdir
     FileUtils.mkdir @config_dir unless Dir.exists? @config_dir
+
+    load_config
+  end
+
+  def init(first_name, last_name)
+    File.open "#{@config_dir}/config", "w" do |f|
+      f.write({:first_name => first_name, :last_name => last_name}.to_yaml)
+    end
+
+    load_config
   end
 
   def add(conf, first_name = nil, last_name = nil)
+    first_name ||= @config[:first_name]
+    last_name ||= @config[:last_name]
+
     File.open "#{@config_dir}/upcoming", 'a' do |f|
       f.write "#{conf},#{first_name},#{last_name}\n"
     end
@@ -14,5 +28,12 @@ class Southy::Config
 
   def remove(conf)
     puts "remove #{conf}"
+  end
+
+  private
+
+  def load_config
+    config_file = "#{@config_dir}/config"
+    @config = YAML.load( IO.read(config_file) ) if File.exists? config_file
   end
 end
--- test/southy/config_test.rb
+++ test/southy/config_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'fileutils'
+require 'yaml'
 
 class ConfigTest < MiniTest::Spec
   
@@ -8,19 +9,30 @@ class ConfigTest < MiniTest::Spec
     before do
       @config_dir = "#{File.dirname(__FILE__)}/../../.config_test_#{rand 1000}"
       FileUtils.remove_entry_secure @config_dir if Dir.exists? @config_dir
+      @config = Southy::Config.new @config_dir
+    end
+
+    describe '#init' do
+      before do
+        @config.init 'First', 'Last'
+        @content = IO.read "#{@config_dir}/config"
+      end
+
+      it 'adds the name' do
+        @content.must_equal({ :first_name => 'First', :last_name => 'Last' }.to_yaml)
+      end
     end
 
     describe '#add' do
       describe 'with name' do
         before do
-          config = Southy::Config.new @config_dir
-          config.add 'ABCDEF', 'First', 'Last'
-          config.add 'GHIJKL', 'One', 'Two'
-          @contents = IO.read "#{@config_dir}/upcoming"
+          @config.add 'ABCDEF', 'First', 'Last'
+          @config.add 'GHIJKL', 'One', 'Two'
+          @content = IO.read "#{@config_dir}/upcoming"
         end
 
         it 'adds the flight' do
-          @contents.must_equal <<EOF
+          @content.must_equal <<EOF
 ABCDEF,First,Last
 GHIJKL,One,Two
 EOF
@@ -28,8 +40,22 @@ EOF
       end
 
       describe 'without name' do
+        before do
+          @config.init 'First', 'Last'
+          @config.add 'ABCDEF'
+          @content = IO.read "#{@config_dir}/upcoming"
+        end
 
+        it 'adds the flight' do
+          @content.must_equal <<EOF
+ABCDEF,First,Last
+EOF
+        end
       end
+    end
+
+    describe '#remove' do
+      
     end
 
     after do
