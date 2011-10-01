a2a2a02cc26029821643b809f318b04f1d3bbc83
Michael Wynholds <mike@carbonfive.com>
2011-10-01 13:23:06 -0700

    Added some timezones

--- lib/southy/timezones.rb
+++ lib/southy/timezones.rb
@@ -1,9 +1,11 @@
 class Southy::Timezones
   LOOKUP =
   {
+    'DEN' => 'America/Denver',
     'HOU' => 'America/Chicago',
     'LAX' => 'America/Los_Angeles',
-    'MSY' => 'America/Chicago'
+    'MSY' => 'America/Chicago',
+    'SFO' => 'America/Los_Angeles'
   }
 
   def self.lookup(code)
