9044e4c46305883a60fadca2006000ec0a6341f3
Michael Wynholds <mike@carbonfive.com>
2011-10-12 17:01:38 -0700

    Smaller airports file

--- lib/southy/airport.rb
+++ lib/southy/airport.rb
@@ -3,6 +3,7 @@ require 'csv'
 class Southy::Airport
 
   @@timezones = {
+          '-11' => 'America/Adak',
           '-10' => 'Pacific/Honolulu',
           '-9'  => 'America/Juneau',
           '-8'  => 'America/Los_Angeles',
@@ -29,7 +30,7 @@ class Southy::Airport
   end
 
   def timezone
-    @@timezones[tz_offset] or raise "Unknown timezone offset: #{self}"
+    @@timezones[tz_offset] or raise "Unknown timezone offset: #{self.name} (#{self.code}): #{tz_offset}"
   end
 
   def to_s
@@ -39,17 +40,15 @@ class Southy::Airport
   private
 
   def self.load_airports
-    datafile = File.dirname(__FILE__) + "/us-airports.dat"
+    datafile = File.dirname(__FILE__) + "/swa-airports.dat"
     File.open(datafile, 'r') do |file|
       file.readlines.each do |line|
         pieces = line.parse_csv
-        if pieces[3] == 'United States'
-          airport = Southy::Airport.new
-          airport.name = pieces[1]
-          airport.code = pieces[4]
-          airport.tz_offset = pieces[9]
-          @@airports[airport.code] = airport
-        end
+        airport = Southy::Airport.new
+        airport.name = pieces[1]
+        airport.code = pieces[4]
+        airport.tz_offset = pieces[9]
+        @@airports[airport.code] = airport
       end
     end
   end
